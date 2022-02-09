460c0a62a3403326cdfcc541d3d71c873e29c5ee
Tom Officer <thomasofficerjr@gmail.com>
2022-02-08 23:49:55 -0800

    looping assignment

---
+++ looping-assignment.sol
@@ -0,0 +1,32 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+contract AddressWallets {
+    uint256 fortune;
+
+    address payable[] investorWallets;
+
+    mapping(address => uint256) investors;
+
+    function payInvestors(address payable wallet, uint256 amount) public {
+        investorWallets.push(wallet);
+        investors[wallet] = amount;
+    }
+
+    function payout() private {
+        for (uint256 i = 0; i < investorWallets.length; i++) {
+            investorWallets[i].transfer(investors[investorWallets[i]]);
+        }
+    }
+
+    constructor() public payable {
+        fortune = msg.value;
+    }
+
+    function makePayment() public {
+        payout();
+    }
+
+    function checkInvestors() public view returns (uint256) {
+        return investorWallets.length;
+    }
+}
